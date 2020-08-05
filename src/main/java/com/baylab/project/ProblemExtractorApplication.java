b49ba61b7279342c609d5291250cd3105c270050
cottory <cottory@naver.com>
2020-08-05 21:01:43 +0900

    init project

---
+++ src/main/java/com/baylab/project/ProblemExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.baylab.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemExtractorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemExtractorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baylab/project/ProblemExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baylab.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemExtractorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
